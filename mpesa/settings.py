7002e65cb6055c8e88e093e0aaaa7550789ac039
Kennedy R. Ngigi <36414555+kennedyngigi4@users.noreply.github.com>
2022-02-21 19:08:32 +0300

    Update settings.py

--- mpesa/settings.py
+++ mpesa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x8^_41ids8_0f0mj=swru5tz9vjwpf4dwfi1p@eah*i*$@xh@4'
+SECRET_KEY = '' #django secret key comes here.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
